e78972fb97675a84145fc05b8b89da040bb08a93
fedorjm <fedorjm@gmail.com>
2022-03-30 10:21:21 -0400

    Update formatting for SQL files

--- 20220309-SurveyResults.sql
+++ 20220309-SurveyResults.sql
@@ -1,20 +1,22 @@
-/* 3/9/2022
+/* 
+3/9/2022
 
 You're consulting for a company, and you've sent out a survey that asks successive qustions randomly. 
 The survey logs data into a table called survey_logging. The schema of the table is:
 
 Column Title	Data Type	Description
-employee_id	integer			employee id of the survey respondant
-action				string			Will be one of the following values, 'view', 'answer', 'skip'
-question_id		integer			ID of the question asked
-answer_id		integer			ID of the answer askedD
-timestamp		integer			time stamp of the action made by respondant
+employee_id	integer	employee id of the survey respondant
+action	string	Will be one of the following values, 'view', 'answer', 'skip'
+question_id	integer	ID of the question asked
+answer_id	integer	ID of the answer askedD
+timestamp	integer	time stamp of the action made by respondant
 
-Using SQL, find which question has the highest response rate. */
+Using SQL, find which question has the highest response rate. 
+*/
 
 select
-	question_id,
-    sum(case action when 'answer' then 1 else 0 end)/count(*) as response_rate 
+ question_id,
+ sum(case action when 'answer' then 1 else 0 end)/count(*) as response_rate 
 from survey_logging
 group by question_id
 order by response_rate desc
--- 20220328-CleanEmployeeData.sql
+++ 20220328-CleanEmployeeData.sql
@@ -3,11 +3,11 @@
 
 Below is a snippet from a table that contains information about employees that work at Company XYZ:  
 
-employee_name		employee_id	date_joined		age		yrs_of_experience  
-Andy						123456			2015-02-15		45		24  
-Beth							789456			NaN					36		15  
-Cindy						654123			2017-05-16		34		14  
-Dale							963852			2018-01-15		25		4  
+employee_name	employee_id	date_joined	age	yrs_of_experience
+Andy	123456	2015-02-15	45	24
+Beth	    789456	NaN	            36	15
+Cindy	654123	2017-05-16	34	14
+Dale	    963852	2018-01-15	25	4
 
 Company XYZ recently migrated database systems causing some of the date_joined records to be NULL.   
 You're told by an analyst in human resources NULL records for the date_joined field indicates the employees joined prior to 2010.   
@@ -17,10 +17,10 @@ Given this, write code to find the number of employees that joined each month. Y
  */  
 
 select
-	-- replace missing dates with Dec 1, 2009 and pull the month
-	month(ifnull(date_joined, '2009-12-01')) as month_joined,
-    -- counting distinct employee IDs avoids issues with same-named employees and duplicate records
-    count(distinct employee_id) as n_employees
+ -- replace missing dates with Dec 1, 2009 and pull the month
+ month(ifnull(date_joined, '2009-12-01')) as month_joined,
+ -- counting distinct employee IDs avoids issues with same-named employees and duplicate records
+ count(distinct employee_id) as n_employees
 from employee_info
 group by month_joined
 order by month_joined;
